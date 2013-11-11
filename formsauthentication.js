b66c45c93f0db23e97d199904716a090c11327ba
sbilodeau <bilodeaustephane@gmail.com>
2013-11-11 14:18:07 -0500

    + Missing UNIT 255 at ticket end + Added missing unencrypted ticket signature before encryption + Automatic AES encryption algo based on key length + Automatic validation hash length based on hash algo

--- formsauthentication.js
+++ formsauthentication.js
@@ -12,10 +12,20 @@ function FormsAuthentication(options) {
 		encryption: 'aes-192-cbc',
 		encryptionKey: null,
 		encryptionBlockSize: 128,
-		encryptionIv: new Buffer(16)
+		encryptionIv: new Buffer(16),
+		ramdomBlockSize : 24
 	};
 
 	this.initialize(options);
+}
+
+function hash(data, options) {
+
+	var hmac = crypto.createHmac(options.validation, options.validationKey);
+
+	hmac.update(data);
+	
+	return hmac.digest();
 }
 
 //============================================================
@@ -26,6 +36,21 @@ FormsAuthentication.prototype.initialize = function(options) {
 
 	if(options && options.validationKey) this.options.validationKey = new Buffer(options.validationKey, 'hex');
 	if(options && options.encryptionKey) this.options.encryptionKey = new Buffer(options.encryptionKey, 'hex');
+
+	if(this.options.encryptionKey && this.options.encryptionKey.length*8 == 128) this.options.encryption = "aes-128-cbc";
+	if(this.options.encryptionKey && this.options.encryptionKey.length*8 == 192) this.options.encryption = "aes-192-cbc";
+	if(this.options.encryptionKey && this.options.encryptionKey.length*8 == 256) this.options.encryption = "aes-256-cbc";
+	if(this.options.encryptionKey)   this.options.ramdomBlockSize = this.options.encryptionKey.length;
+
+	if(options && options.encryption) this.options.encryption = options.encryption;
+	if(options && options.validation) this.options.validation = options.validation;
+  
+	if(this.options.validation=="SHA1")   this.options.validationHashLength = 160;
+	if(this.options.validation=="SHA2")   this.options.validationHashLength = 256;
+	if(this.options.validation=="SHA256") this.options.validationHashLength = 256;
+	if(this.options.validation=="SHA512") this.options.validationHashLength = 512;
+
+	if(options && options.validationHashLength) this.options.validation = options.validationHashLength;
 
 	this.options.encryptionIv.fill(0);
 };
@@ -38,7 +63,7 @@ FormsAuthentication.prototype.encrypt = function(ticket) {
 
 	// SERIALIZE
 
-	var writer = new BufferWriter(crypto.randomBytes(24));
+	var writer = new BufferWriter(new Buffer([]));
 
 	writer.writeUInt8(1);
 	writer.writeUInt8(ticket.version);
@@ -49,25 +74,27 @@ FormsAuthentication.prototype.encrypt = function(ticket) {
 	writer.writeString(ticket.name);
 	writer.writeString(ticket.userData);
 	writer.writeString(ticket.cookiePath);
+	writer.writeUInt8(255);
 
-	var decryptedTicket = writer.final();
+	// SIGN RAW
+
+	var decryptedTicketData = writer.final();
+	var decryptedTicketHash = hash(decryptedTicketData, this.options)
+	var decryptedTicket     = Buffer.concat([decryptedTicketData, decryptedTicketHash]);
 
 	// ENCRYPT
 
 	var cipher = crypto.createCipheriv(this.options.encryption, this.options.encryptionKey, this.options.encryptionIv);
 
-	var buffer1 = cipher.update(decryptedTicket);
+	var iv      = crypto.randomBytes(this.options.ramdomBlockSize);
+	var buffer1 = cipher.update(Buffer.concat([iv, decryptedTicket]));
 	var buffer2 = cipher.final();
 
+	// SIGN ENCRYPTED
+
 	var encryptedTicketData = Buffer.concat([buffer1, buffer2]);
-
-	// SIGN
-
-	var hmac = crypto.createHmac(this.options.validation, this.options.validationKey);
-	hmac.update(encryptedTicketData);
-	var hash = hmac.digest();
-
-	var encryptedTicket = Buffer.concat([encryptedTicketData, hash]);
+	var encryptedTicketHash = hash(encryptedTicketData, this.options);
+	var encryptedTicket     = Buffer.concat([encryptedTicketData, encryptedTicketHash]);
 
 	return encryptedTicket.toString('hex').toUpperCase();
 };
@@ -80,15 +107,13 @@ FormsAuthentication.prototype.decrypt = function(encryptedTicket) {
 
 	var data = new Buffer(encryptedTicket, 'hex');
 	var encryptedTicketData = data.slice(0, data.length - this.options.validationHashLength / 8);
-	var encryptedTicketSignature = data.slice(data.length - this.options.validationHashLength / 8);
+	var encryptedTicketHash = data.slice(   data.length - this.options.validationHashLength / 8);
 
-	// CHECK SIGNATURE
+	// CHECK ENCRYPTED SIGNATURE
 
-	var hmac = crypto.createHmac(this.options.validation, this.options.validationKey);
-	hmac.update(encryptedTicketData);
-	var hash = hmac.digest();
+	var computedHash = hash(encryptedTicketData, this.options);
 
-	if(hash.toString()!=encryptedTicketSignature.toString())
+	if(computedHash.toString('hex') != encryptedTicketHash.toString('hex'))
 		throw new Error('encryptedTicket is of an invalid format.');
 
 	// DECRYPT
@@ -98,7 +123,17 @@ FormsAuthentication.prototype.decrypt = function(encryptedTicket) {
 	var buffer1 = decipher.update(encryptedTicketData);
 	var buffer2 = decipher.final();
 
-	var decryptedTicket = Buffer.concat([buffer1, buffer2]).slice(24);
+	var decryptedTicket = Buffer.concat([buffer1, buffer2]).slice(this.options.ramdomBlockSize);
+
+	var decryptedTicketData = decryptedTicket.slice(0, decryptedTicket.length - this.options.validationHashLength / 8);
+	var decryptedTicketHash = decryptedTicket.slice(   decryptedTicket.length - this.options.validationHashLength / 8);
+
+	// CHECK SIGNATURE 1
+
+	var computedHash = hash(decryptedTicketData, this.options);
+
+	if(computedHash.toString('hex') != decryptedTicketHash.toString('hex'))
+		throw new Error('encryptedTicket is of an invalid format.');
 
 	// DESERIALIZE
 
@@ -114,10 +149,10 @@ FormsAuthentication.prototype.decrypt = function(encryptedTicket) {
 		name:         reader.readString(),
 		userData:     reader.readString(),
 		cookiePath:   reader.readString(),
+		z:            reader.readUInt8(),	// 255
 	};
 
 	return ticket;
 };
 
 exports = module.exports = new FormsAuthentication();
-
